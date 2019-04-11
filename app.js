3ca0847f08f2dbbd30e04e08140851c57624800c
Termanteus <bomcon123456@gmail.com>
2019-04-11 09:30:26 +0700

    Install Template Engine

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug')
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,14 @@ const express = require('express');
 const path = require('path');
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
 
 const router = express.Router();
 
 // .use() -> Add new middleware
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log(adminData.products);
+    res.render('shop');
 });
 
 module.exports = router;
